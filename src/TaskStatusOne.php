4d1d5831df4e9243542229c517fd8c5b39b410fa
Natalia Sevostyanova <sevostyanova@gmail.com>
2019-11-11 20:54:24 +0300

    Переименовала корневую папку. тесты testTaskStatus и unloadtestTaskStatus

--- src/TaskStatusOne.php
+++ src/TaskStatusOne.php
@@ -1,11 +1,10 @@
 <?php
     /**
-      * File for testing if Class works right way
-      */
-    /*
-     * loading function for Class TaskStatusOne
-     */
-    //use YiiTaskForce\TaskStatusOne;
+    *
+    * Класс для определения активного статуса задания
+    * @author <sevostyanova@gmail.com>
+    */
+
     namespace YiiTaskForce;
 
     class TaskStatusOne
@@ -60,10 +59,6 @@
             ];
             return $this->statuses = $statuses;
         }
-    /*public function __construct ()
-        {
-
-        }*/
 
     public function getActiveStatus(string $action)
         { //получаем активный статус
@@ -80,16 +75,4 @@
             }
                 return $this->activeStatus = $activeStatus;
         }
-    }
-
-    $strategy = new TaskStatusOne();
-
-    //настройки
-    assert_options(ASSERT_ACTIVE, 1);
-    assert_options(ASSERT_WARNING, 0);
-
-    assert($strategy->getActiveStatus(TaskStatusOne::ACTION_ORDER) == TaskStatus::STATUS_NEW, 'problem with order action');
-    assert($strategy->getActiveStatus(TaskStatusOne::ACTION_CANCEL) == TaskStatus::STATUS_CANCEL, 'problem with cancel action');
-    assert($strategy->getActiveStatus(TaskStatusOne::ACTION_DO) == TaskStatus::STATUS_INPROCESS, 'problem with do action');
-    assert($strategy->getActiveStatus(TaskStatusOne::ACTION_FINISH) == TaskStatus::STATUS_FINISH, 'problem with finish action');
-    assert($strategy->getActiveStatus(TaskStatusOne::ACTION_PAY) == TaskStatus::STATUS_FINISH, 'problem with pay action');
+}
