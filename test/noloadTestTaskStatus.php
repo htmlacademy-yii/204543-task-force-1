7f21cbc311fac31382f19ab79a06e0b9fc5eb264
Natalia Sevostyanova <sevostyanova@gmail.com>
2019-11-14 15:01:27 +0300

    имя проверяемого класса TaskStatus

--- test/noloadTestTaskStatus.php
+++ test/noloadTestTaskStatus.php
@@ -3,18 +3,18 @@
       * File for testing if Class works right way
       */
     /*
-     * loading function for Class TaskStatusOne
+     * loading function for Class TaskStatus
      */
-    use YiiTaskForce\TaskStatusOne;
+    use YiiTaskForce\TaskStatus;
 
-    $strategy = new TaskStatusOne();
+    $strategy = new TaskStatus();
 
     //настройки
     assert_options(ASSERT_ACTIVE, 1);
     assert_options(ASSERT_WARNING, 0);
 
-    assert($strategy->getActiveStatus(TaskStatusOne::ACTION_ORDER) == TaskStatusOne::STATUS_NEW, 'problem with order action');
-    assert($strategy->getActiveStatus(TaskStatusOne::ACTION_CANCEL) == TaskStatusOne::STATUS_CANCEL, 'problem with cancel action');
-    assert($strategy->getActiveStatus(TaskStatusOne::ACTION_DO) == TaskStatusOne::STATUS_INPROCESS, 'problem with do action');
-    assert($strategy->getActiveStatus(TaskStatusOne::ACTION_FINISH) == TaskStatusOne::STATUS_FINISH, 'problem with finish action');
-    assert($strategy->getActiveStatus(TaskStatusOne::ACTION_PAY) == TaskStatusOne::STATUS_FINISH, 'problem with pay action');
+    assert($strategy->getActiveStatus(TaskStatus::ACTION_ORDER) == TaskStatus::STATUS_NEW, 'problem with order action');
+    assert($strategy->getActiveStatus(TaskStatus::ACTION_CANCEL) == TaskStatus::STATUS_CANCEL, 'problem with cancel action');
+    assert($strategy->getActiveStatus(TaskStatus::ACTION_DO) == TaskStatus::STATUS_INPROCESS, 'problem with do action');
+    assert($strategy->getActiveStatus(TaskStatus::ACTION_FINISH) == TaskStatus::STATUS_FINISH, 'problem with finish action');
+    assert($strategy->getActiveStatus(TaskStatus::ACTION_PAY) == TaskStatus::STATUS_FINISH, 'problem with pay action');
