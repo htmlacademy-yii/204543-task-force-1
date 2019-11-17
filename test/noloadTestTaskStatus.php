4fd4d126386789755886ce6e9e6760ca612c9264
Natalia Sevostyanova <sevostyanova@gmail.com>
2019-11-17 20:27:40 +0300

    Action_pay - Status_paid

--- test/noloadTestTaskStatus.php
+++ test/noloadTestTaskStatus.php
@@ -21,4 +21,4 @@
     assert($strategy->getActiveStatus(TaskStatus::ACTION_CANCEL) == TaskStatus::STATUS_CANCEL, 'problem with cancel action');
     assert($strategy->getActiveStatus(TaskStatus::ACTION_DO) == TaskStatus::STATUS_INPROCESS, 'problem with do action');
     assert($strategy->getActiveStatus(TaskStatus::ACTION_FINISH) == TaskStatus::STATUS_FINISH, 'problem with finish action');
-    assert($strategy->getActiveStatus(TaskStatus::ACTION_PAY) == TaskStatus::STATUS_FINISH, 'problem with pay action');
+    assert($strategy->getActiveStatus(TaskStatus::ACTION_PAY) == TaskStatus::STATUS_PAID, 'problem with pay action');
